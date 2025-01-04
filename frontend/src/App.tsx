8d5f8233ad1d06321e00600dd8cdbe1d79a84575
gtn-74 <gtn.r74@gmail.com>
2025-01-04 23:18:35 +0900

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return <h1>Hello, TypeScript with React!</h1>;
+};
+
+export default App;
+
+// const App = () => <h1>Hello, React!</h1>;
+
+// const container = document.getElementById('root');
+// const root = createRoot(container!);
+// root.render(<App />);
